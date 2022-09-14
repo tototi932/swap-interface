6d82ff02490185232bfdc5a3625a39dba7192c3e
doge <110700029+tototi932@users.noreply.github.com>
2022-09-13 23:14:18 -0400

    Update index.esm.js

--- vital-folders/uikit/dist/index.esm.js
+++ vital-folders/uikit/dist/index.esm.js
@@ -3126,15 +3126,15 @@ var links = [
             },
             {
                 label: "Github",
-                href: "https://github.com/Bridgeswap-Dex",
+                href: "https://github.com/Doge-Gold-Floki",
             },
             {
                 label: "Docs",
-                href: "https://docs.bridgeswap.app",
+                href: "https://doge-gold-floki-finance.gitbook.io/dgf/",
             },
             {
                 label: "Blog",
-                href: "https://bridgeswap.medium.com",
+                href: "https://medium.com/@Doge-Gold-Floki",
             },
         ],
     },
@@ -3143,22 +3143,22 @@ var socials = [
     {
         label: "Github",
         icon: "GithubIcon",
-        href: "https://github.com/Bridgeswap-Dex",
+        href: "https://github.com/Doge-Gold-Floki",
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/bridgeswap_dex",
+        href: "https://twitter.com/Doge_Gold_Floki",
     },
     {
         label: "Medium",
         icon: "MediumIcon",
-        href: "https://bridgeswap.medium.com",
+        href: "https://medium.com/@Doge-Gold-Floki",
     },
     {
         label: "Gitbook",
         icon: "GitbookIcon",
-        href: "https://bridgeswap-exchange-1.gitbook.io/bridgeswap/",
+        href: "https://doge-gold-floki-finance.gitbook.io/dgf/",
     },
     {
         label: "Telegram",
@@ -3166,7 +3166,7 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/bridgeswap",
+                href: "https://t.me/Doge_Gold_Floki",
             },
             // {
             //   label: "Bahasa Indonesia",
